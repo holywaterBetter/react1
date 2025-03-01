f276f75bf24e64332c782bb307e73bcbe94a3a1b
holywaterBetter <holywater.better@gmail.com>
2025-03-01 15:54:39 +0900

    리스트 랜더링 - 챌린지 3 #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
-          </ul>
-        </div>
-      ))};
+      {recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} /> )}
     </div>
   );
+}
+
+export function Recipe({ recipe }) {
+  return <>
+    <h2>{recipe.name}</h2>
+    <ul>
+      {recipe.ingredients.map(ingredient =>
+        <li key={ingredient}>
+          {ingredient}
+        </li>
+      )}
+    </ul>
+  </>
 }
 
 export const recipes = [{
