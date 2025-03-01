d76a37ed947183996e38fbd65c610a3acbc146cb
holywaterBetter <holywater.better@gmail.com>
2025-03-01 16:03:34 +0900

    리스트 랜더링 - 챌린지 4 #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-export default function RecipeList() {
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.'
+  ]
+};
+
+export default function Poem() {
   return (
-    <div>
-      <h1>Recipes</h1>
-      {recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} /> )}
-    </div>
+    <article>
+      {poem.lines.map((line, index) => <>
+        <p key={index}>{line}</p>
+        {index < poem.lines.length - 1 && <hr />}
+      </>
+      )}
+    </article>
   );
 }
-
-export function Recipe({ recipe }) {
-  return <>
-    <h2>{recipe.name}</h2>
-    <ul>
-      {recipe.ingredients.map(ingredient =>
-        <li key={ingredient}>
-          {ingredient}
-        </li>
-      )}
-    </ul>
-  </>
-}
-
-export const recipes = [{
-  id: 'greek-salad',
-  name: 'Greek Salad',
-  ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta']
-}, {
-  id: 'hawaiian-pizza',
-  name: 'Hawaiian Pizza',
-  ingredients: ['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple']
-}, {
-  id: 'hummus',
-  name: 'Hummus',
-  ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini']
-}];
