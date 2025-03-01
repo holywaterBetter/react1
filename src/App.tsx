25e02bed0855df14a35c98757656606fa4672c66
holywaterBetter <holywater.better@gmail.com>
2025-03-02 07:35:15 +0900

    feat(#4): State 관리하기 - input 다루기 챌린지1

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-let nextId = 3;
-const initialTodos = [
-  { id: 0, title: 'Buy milk', done: true },
-  { id: 1, title: 'Eat tacos', done: false },
-  { id: 2, title: 'Brew tea', done: false },
-];
-
-export default function TaskApp() {
-  const [todos, setTodos] = useState(
-    initialTodos
-  );
-
-  function handleAddTodo(title) {
-    setTodos(todos.concat({
-      id: nextId++,
-      title: title,
-      done: false
-    }))
-  }
-
-  function handleChangeTodo(nextTodo) {
-    setTodos(todos.map(todo => {
-      if(todo.id === nextTodo.id) {
-        return {
-          ...todo,
-          title: nextTodo.title,
-          done: nextTodo.done,
-        }
-      }
-      return todo
-    }))
-  }
-
-  function handleDeleteTodo(todoId) {
-    setTodos(todos.filter(todo => todo.id !== todoId));
-  }
-
+export default function Picture() {
+  const [classes, setClasses] = useState({
+    parent: "background background--active",
+    child: "picture"
+  })
   return (
-    <>
-      <AddTodo
-        onAddTodo={handleAddTodo}
-      />
-      <TaskList
-        todos={todos}
-        onChangeTodo={handleChangeTodo}
-        onDeleteTodo={handleDeleteTodo}
-      />
-    </>
-  );
-}
-
-export function AddTodo({ onAddTodo }) {
-  const [title, setTitle] = useState('');
-  return (
-    <>
-      <input
-        placeholder="Add todo"
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-      />
-      <button onClick={() => {
-        setTitle('');
-        onAddTodo(title);
-      }}>Add</button>
-    </>
-  )
-}
-
-export function TaskList({
-  todos,
-  onChangeTodo,
-  onDeleteTodo
-}) {
-  return (
-    <ul>
-      {todos.map(todo => (
-        <li key={todo.id}>
-          <Task
-            todo={todo}
-            onChange={onChangeTodo}
-            onDelete={onDeleteTodo}
-          />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function Task({ todo, onChange, onDelete }) {
-  const [isEditing, setIsEditing] = useState(false);
-  let todoContent;
-  if (isEditing) {
-    todoContent = (
-      <>
-        <input
-          title='todo'
-          value={todo.title}
-          onChange={e => {
-            onChange({
-              ...todo,
-              title: e.target.value
-            });
-          }} />
-        <button onClick={() => setIsEditing(false)}>
-          Save
-        </button>
-      </>
-    );
-  } else {
-    todoContent = (
-      <>
-        {todo.title}
-        <button onClick={() => setIsEditing(true)}>
-          Edit
-        </button>
-      </>
-    );
-  }
-  return (
-    <label>
-      <input
-        type="checkbox"
-        checked={todo.done}
-        onChange={e => {
-          onChange({
-            ...todo,
-            done: e.target.checked
-          });
+    <div className={classes.parent}>
+      <img
+        className={classes.child}
+        onClick={() => {
+          if(classes.parent.includes("background--active")){
+            setClasses({
+              parent: "background",
+              child: "picture picture--inactive"
+            })
+          } else {
+            setClasses({
+              parent: "background background--active",
+              child: "picture"
+            })
+          }
         }}
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
       />
-      {todoContent}
-      <button onClick={() => onDelete(todo.id)}>
-        Delete
-      </button>
-    </label>
+    </div>
   );
 }
