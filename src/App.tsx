851298566e030a91ad470c7bd8edb54bda542154
holywaterBetter <holywater.better@gmail.com>
2025-03-02 07:52:26 +0900

    feat(#4): State 관리하기 - input 다루기 챌린지2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,49 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
-export default function Picture() {
-  const [classes, setClasses] = useState({
-    parent: "background background--active",
-    child: "picture"
-  })
-  return (
-    <div className={classes.parent}>
-      <img
-        className={classes.child}
-        onClick={() => {
-          if(classes.parent.includes("background--active")){
-            setClasses({
-              parent: "background",
-              child: "picture picture--inactive"
-            })
-          } else {
-            setClasses({
-              parent: "background background--active",
-              child: "picture"
-            })
-          }
-        }}
-        alt="Rainbow houses in Kampung Pelangi, Indonesia"
-        src="https://i.imgur.com/5qwVYb1.jpeg"
+export default function EditProfile() { 
+  const [firstNameInput, setFirstNameInput] = useState("");
+  const [lastNameInput, setLastNameInput] = useState("");
+  const [isEdit, setIsEdit] = useState(true);
+
+  function firstText() {
+    if(isEdit) {
+      return <input
+        title="firstNameInput"
+        value={firstNameInput}
+        onChange= {e => setFirstNameInput(e.target.value)}
       />
+    }
+    return <b>{firstNameInput}</b>
+  }
+
+  function lastText() {
+    if(isEdit) {
+      return <input
+        title="lastNameInput"
+        value={lastNameInput}
+        onChange= {e => setLastNameInput(e.target.value)}
+      />
+    }
+    return <b>{lastNameInput}</b>
+  }
+
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault();
+        setIsEdit(!isEdit);
+      }}>
+        <label>
+          First name: {firstText()}
+        </label>
+        <label>
+          Last name: {lastText()}
+        </label>
+        <button type="submit">
+          Edit Profile
+        </button>
+        <p><i>Hello, {firstNameInput} {lastNameInput}!</i></p>
+      </form>
     </div>
   );
 }
