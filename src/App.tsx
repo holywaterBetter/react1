5983b1187614c311ab44c60c61ba3823d36ac043
holywaterBetter <holywater.better@gmail.com>
2025-03-02 22:52:12 +0900

    feat(#4): 탈출구 - Ref로 DOM 조작하기 챌린지 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import { useRef } from "react";
+import { useRef, useState } from 'react';
 
-export default function Page() {
+export default function CatFriends() {  
+  const [index, setIndex] = useState(0);
   const ref = useRef(null);
   return (
     <>
       <nav>
-        <button onClick={()=>{
-          ref.current.focus();
-        }}>Search</button>
+        <button onClick={() => {
+          if (index < catList.length - 1) {
+            setIndex(index + 1);
+          } else {
+            setIndex(0);
+          }
+                ref.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center'
+          });
+        }}>
+          Next
+        </button>
       </nav>
-      <input
-        ref={ref}
-        placeholder="Looking for something?"
-      />
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li key={cat.id} ref={index === i ? ref : null}>
+              <img
+                className={
+                  index === i ?
+                    'active' :
+                    ''
+                }
+                src={cat.imageUrl}
+                alt={'Cat #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://loremflickr.com/250/200/cat?lock=' + i
+  });
+}
+
