741037755dbfb2bc4a3bf480e0f3a56405ea7c89
holywaterBetter <holywater.better@gmail.com>
2025-03-02 21:57:34 +0900

    feat(#4): 탈출구 - Ref로 값 참조하기 챌린지 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import AddTask from './AddTask';
-import TaskContext from './TaskContext';
-import TaskList from './TaskList';
+import { useState, useRef } from 'react';
 
-export default function TaskApp() {
+export default function Chat() {
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  let timeoutID = useRef<NodeJS.Timeout>(undefined);
+
+  function handleSend() {
+    setIsSending(true);
+    timeoutID.current = setTimeout(() => {
+      alert('Sent!');
+      setIsSending(false);
+    }, 3000);
+  }
+
+  function handleUndo() {
+    setIsSending(false);
+    clearTimeout(timeoutID.current);
+  }
+
   return (
-    <TaskContext>
-      <h1>Day off in Kyoto</h1>
-      <AddTask/>
-      <TaskList/>
-    </TaskContext>
+    <>
+      <input
+        title='text'
+        disabled={isSending}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button
+        disabled={isSending}
+        onClick={handleSend}>
+        {isSending ? 'Sending...' : 'Send'}
+      </button>
+      {isSending &&
+        <button onClick={handleUndo}>
+          Undo
+        </button>
+      }
+    </>
   );
 }
