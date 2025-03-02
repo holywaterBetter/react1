7bfb533d5975e1df6ed336a8306b26ca073b6ba9
holywaterBetter <holywater.better@gmail.com>
2025-03-02 21:58:45 +0900

    feat(#4): 탈출구 - Ref로 값 참조하기 챌린지 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
-export default function Chat() {
-  const [text, setText] = useState('');
-  const [isSending, setIsSending] = useState(false);
-  let timeoutID = useRef<NodeJS.Timeout>(undefined);
-
-  function handleSend() {
-    setIsSending(true);
-    timeoutID.current = setTimeout(() => {
-      alert('Sent!');
-      setIsSending(false);
-    }, 3000);
-  }
-
-  function handleUndo() {
-    setIsSending(false);
-    clearTimeout(timeoutID.current);
-  }
+export default function Toggle() {
+  const [isOn, setIsOn] = useState(false);
 
   return (
-    <>
-      <input
-        title='text'
-        disabled={isSending}
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button
-        disabled={isSending}
-        onClick={handleSend}>
-        {isSending ? 'Sending...' : 'Send'}
-      </button>
-      {isSending &&
-        <button onClick={handleUndo}>
-          Undo
-        </button>
-      }
-    </>
+    <button onClick={() => {
+      setIsOn(!isOn);
+    }}>
+      {isOn ? 'On' : 'Off'}
+    </button>
   );
 }
