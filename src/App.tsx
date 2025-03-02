e13391727b5e3eb3056ba5bfe65c551e0c2ade97
holywaterBetter <holywater.better@gmail.com>
2025-03-03 06:30:57 +0900

    feat(#4): 탈출구 - Effect으로 동기화하기 챌린지 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import { useState, useRef, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
-function VideoPlayer({ src, isPlaying }) {
+export default function MyInput({ value, onChange }) {
   const ref = useRef(null);
 
-  useEffect(() => {
-    if (isPlaying) {
-      ref.current.play();  
-    } else {
-      ref.current.pause();
-    }
-  })
-  
-  return <video ref={ref} src={src} loop playsInline />;
-}
+  useEffect(()=>{
+    ref.current.focus()
+  },[]);
 
-export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
   return (
-    <>
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? '일시정지' : '재생'}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
-    </>
+    <input
+      ref={ref}
+      value={value}
+      onChange={onChange}
+    />
   );
 }
